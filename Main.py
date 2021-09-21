023bef513c0e3a13372e14d91b185e65161857af
Gaeduck <gms8757@naver.com>
2021-09-21 18:40:49 +0900

    var name change

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 #기본설정
 import tkinter as tk
 
-ScereenNum = 0 #화면에 표시될 변수
+screenNum = 0 #화면에 표시될 변수
 sign = {'+':1, '-':2, '/':3, '*':4, 'C':5, '=':6} #기호에 맞는 계산을 하게끔 딕셔너리
 saveNum = 0 #수를 저장할 변수
 SelectSign = 0 #
@@ -9,48 +9,48 @@ SelectSign = 0 #
 
 #화면 클리어
 def clear():
-    global ScereenNum,saveNum,SelectSign #전역으로 호출
-    ScereenNum = 0 #초기화
+    global screenNum,saveNum,SelectSign #전역으로 호출
+    screenNum = 0 #초기화
     saveNum = 0
     SelectSign = 0
-    str_Value.set(ScereenNum) #화면에 표시
+    str_Value.set(screenNum) #화면에 표시
 
 #숫자 감지
 def NumCheck(value):
-    global ScereenNum
-    ScereenNum = (ScereenNum*10) + value
-    str_Value.set(ScereenNum)
+    global screenNum
+    screenNum = (screenNum*10) + value
+    str_Value.set(screenNum)
 
 
 #계산
 def Calculate(value):
-    global ScereenNum,sign,saveNum,SelectSign
+    global screenNum,sign,saveNum,SelectSign
     op = sign[value] #기능선택
     
     if op == 5: #C
         clear()
     
-    elif ScereenNum == 0: 
+    elif screenNum == 0: 
         SelectSign = 0
     
     elif SelectSign == 0: 
         SelectSign = op
-        saveNum = ScereenNum
-        ScereenNum = 0
-        str_Value.set(str(ScereenNum))
+        saveNum = screenNum
+        screenNum = 0
+        str_Value.set(str(screenNum))
     
     elif op == 6: #계산
         if SelectSign == 1: #+
-            ScereenNum = saveNum + ScereenNum
+            screenNum = saveNum + screenNum
         if SelectSign == 2: #-
-            ScereenNum = saveNum - ScereenNum
+            screenNum = saveNum - screenNum
         if SelectSign == 3: #/
-            ScereenNum = saveNum / ScereenNum
+            screenNum = saveNum / screenNum
         if SelectSign == 4: #*
-            ScereenNum = saveNum * ScereenNum
+            screenNum = saveNum * screenNum
 
-        str_Value.set(str(ScereenNum))
-        ScereenNum = 0
+        str_Value.set(str(screenNum))
+        screenNum = 0
         saveNum = 0
         SelectSign = 0
     
@@ -71,7 +71,7 @@ window.title("대충 계산기")
 
 #수 입력 공간
 str_Value = tk.StringVar()
-str_Value.set(str(ScereenNum))
+str_Value.set(str(screenNum))
 input = tk.Entry(window, textvariable=str_Value, justify='right', bg='white',fg='red')
 input.grid(column=0,row=0,columnspan = 5, ipadx=120,ipady=30)
 
